e7dbf4df8009ba16b89909c153b7f8b72aedc3cc
Raúl Díez <rdiez@begar.es>
2015-04-21 19:43:07 +0200

    maestro de localizaciones completo

---
+++ resources/lang/es/localizaciones/message.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Language file for localizacion error/success messages
+*
+*/
+
+return array(
+
+    'empresa_exists'        => 'la localizacion ya existe!',
+    'empresa_not_found'     => 'La localizacion [:id] no existe.',
+    'empresa_name_required' => 'El campo es obligatorio',
+
+    'success' => array(
+        'create' => 'La localizacion fue creada correctamente.',
+        'update' => 'La localizacion fue actualizada correctamente.',
+        'delete' => 'La localizacion fue borrada correctamente.',
+    ),
+
+    'delete' => array(
+        'create' => 'ha habido un problema al crer la localizacion, por favor intentelo de nuevo más tarde.',
+        'update' => 'ha habido un problema al actualizar la localizacion, por favor intentelo de nuevo más tarde.',
+        'delete' => 'ha habido un problema al borrar la localizacion, por favor intentelo de nuevo más tarde.',
+    ),
+
+    'error' => array(
+        'empresa_exists' => 'la localizacion ya existe con ese nombre. Los nombres de localizacion deben ser unicos',
+    ),
+
+);
---
+++ resources/lang/es/localizaciones/modal.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Language file for group delete modal
+*
+*/
+return array(
+
+    'title'         => 'Borrar Localización',
+    'body'			=> '¿Estás seguro de borrar esta localización? La operación no tiene vuelta atrás.',
+    'cancel'		=> 'Cancelar',
+    'confirm'		=> 'Borrar',
+
+);
